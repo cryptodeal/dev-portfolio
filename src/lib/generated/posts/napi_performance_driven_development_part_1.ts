a438e99f348f8724ffc878673714d29898732aa0
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2023-01-15 14:39:43 -0600

    generate images; twitter cache borking img for posts

--- src/lib/generated/posts/napi_performance_driven_development_part_1.ts
+++ src/lib/generated/posts/napi_performance_driven_development_part_1.ts
@@ -6,20 +6,20 @@ import ogSquareImage from '$lib/assets/blog/napi_performance_driven_development_
 import twitterImage from '$lib/assets/blog/napi_performance_driven_development_part_1/napi_performance_twitter.jpg';
 const { height, src, width } = meta;
 const data = {
-	slug: 'napi_performance_driven_development_part_1',
-	alt: 'Depiction of the bidirectional flow of data from Javascript to C++ and vice versa.',
-	width,
-	height,
-	src,
-	sources: [
-		{ srcset: srcsetwebp, type: 'image/webp' },
-		{ srcset: srcsetauto, type: 'image/jpeg' }
-	],
-	dominantColour: '#282828',
-	placeholder:
-		'data:image/jpeg;base64,/9j/2wBDAAwREhMUFBUUFBofGhUaHiIcGhohKC4jJB4hMzg/PTouKC5CRFpMPi5XRTc3VmBRVlpgZmRkQklveXFmeFtlaWf/2wBDAQgMDQwNDw8PDxFgEBIaVmBaYFpgY2NjY2BjYGBiY2NjY2NjY2NjY2NjY2JjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAGAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQG/8QAFAEBAAAAAAAAAAAAAAAAAAAAAf/aAAwDAQACEAMQAAAAxkYH/8QAGRAAAgMBAAAAAAAAAAAAAAAAAgMBECFB/9oACAEBAAE/AFkMKPN7X//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z',
-	ogImage,
-	ogSquareImage,
-	twitterImage
+  slug: 'napi_performance_driven_development_part_1',
+  alt: 'Depiction of the bidirectional flow of data from Javascript to C++ and vice versa.',
+  width,
+  height,
+  src,
+  sources: [
+    { srcset: srcsetwebp, type: 'image/webp' },
+    { srcset: srcsetauto, type: 'image/jpeg' },
+  ],
+  dominantColour: '#282828',
+  placeholder:
+    'data:image/jpeg;base64,/9j/2wBDAAwREhMUFBUUFBofGhUaHiIcGhohKC4jJB4hMzg/PTouKC5CRFpMPi5XRTc3VmBRVlpgZmRkQklveXFmeFtlaWf/2wBDAQgMDQwNDw8PDxFgEBIaVmBaYFpgY2NjY2BjYGBiY2NjY2NjY2NjY2NjY2JjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAGAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQG/8QAFAEBAAAAAAAAAAAAAAAAAAAAAf/aAAwDAQACEAMQAAAAxkYH/8QAGRAAAgMBAAAAAAAAAAAAAAAAAgMBECFB/9oACAEBAAE/AFkMKPN7X//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z',
+  ogImage,
+  ogSquareImage,
+  twitterImage,
 };
 export { data as default };
